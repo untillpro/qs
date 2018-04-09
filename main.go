9aabca26d6950267cdfa0f30b187a4462dbf349a
maxim-ge <maxim.ge@gmail.com>
2018-04-09 19:13:58 +0300

    misc

--- main.go
+++ main.go
@@ -2,9 +2,38 @@ package main
 
 import (
 	"fmt"
+
+	cobra "github.com/spf13/cobra"
 )
 
-func main(){
-	fmt.Println("Hello, world")
+func main() {
+
+	var rootCmd = &cobra.Command{
+		Use:   "qg",
+		Short: "Quick git wrapper",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd Run with args: %v\n", args)
+		},
+	}
+
+	var uploadCmd = &cobra.Command{
+		Use:   "u",
+		Short: "Upload sources",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	var downloadCmd = &cobra.Command{
+		Use:   "d",
+		Short: "Download sources",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside download with args: %v\n", args)
+		},
+	}
+
+	rootCmd.AddCommand(uploadCmd)
+	rootCmd.AddCommand(downloadCmd)
+
+	rootCmd.Execute()
 
 }
