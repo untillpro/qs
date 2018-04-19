a5d2dd8f74d6991543fe7507780c1793b4b76cdc
maxim-ge <maxim.ge@gmail.com>
2018-04-19 17:51:48 +0300

    misc

--- utils/PipedExec.go
+++ utils/PipedExec.go
@@ -24,49 +24,49 @@ type pipedCmd struct {
 	cmd               *exec.Cmd
 }
 
-func (pExec *PipedExec) command(name string, stderrRedirection int, args ...string) *PipedExec {
+func (Self *PipedExec) command(name string, stderrRedirection int, args ...string) *PipedExec {
 	cmd := exec.Command(name, args...)
-	lastIdx := len(pExec.cmds) - 1
+	lastIdx := len(Self.cmds) - 1
 	if lastIdx > -1 {
 		var err error
-		cmd.Stdin, err = pExec.cmds[lastIdx].cmd.StdoutPipe()
+		cmd.Stdin, err = Self.cmds[lastIdx].cmd.StdoutPipe()
 		PanicIfError(err)
 	}
-	pExec.cmds = append(pExec.cmds, &pipedCmd{stderrRedirection, cmd})
-	return pExec
+	Self.cmds = append(Self.cmds, &pipedCmd{stderrRedirection, cmd})
+	return Self
 }
 
 // Command adds a command to a pipe
-func (pExec *PipedExec) Command(name string, args ...string) *PipedExec {
-	return pExec.command(name, StderrRedirectNone, args...)
+func (Self *PipedExec) Command(name string, args ...string) *PipedExec {
+	return Self.command(name, StderrRedirectNone, args...)
 }
 
 // Wd sets working directory for the last command
-func (pExec *PipedExec) Wd(wd string) *PipedExec {
-	pipedCmd := pExec.cmds[len(pExec.cmds)-1]
+func (Self *PipedExec) Wd(wd string) *PipedExec {
+	pipedCmd := Self.cmds[len(Self.cmds)-1]
 	pipedCmd.cmd.Dir = wd
-	return pExec
+	return Self
 }
 
 // Run starts the pipe
-func (pExec *PipedExec) Run(out io.Writer, err io.Writer) {
-	for _, cmd := range pExec.cmds {
+func (Self *PipedExec) Run(out io.Writer, err io.Writer) {
+	for _, cmd := range Self.cmds {
 		if cmd.stderrRedirection == StderrRedirectNone {
 			cmd.cmd.Stderr = err
 		}
 	}
-	lastIdx := len(pExec.cmds) - 1
+	lastIdx := len(Self.cmds) - 1
 	if lastIdx < 0 {
 		return
 	}
-	pExec.cmds[lastIdx].cmd.Stdout = out
+	Self.cmds[lastIdx].cmd.Stdout = out
 
-	for _, cmd := range pExec.cmds {
+	for _, cmd := range Self.cmds {
 		err := cmd.cmd.Start()
 		if nil != err {
 			panic("Failed:" + cmd.cmd.Path)
 		}
 	}
 
-	pExec.cmds[lastIdx].cmd.Wait()
+	Self.cmds[lastIdx].cmd.Wait()
 }
