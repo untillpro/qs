ea6eaf0a94fda6507535637b4a3393622c809b6a
maxim-ge <maxim.ge@gmail.com>
2018-11-29 11:27:32 +0300

    Revert "Await for each cmd completion added (#2)"
    This reverts commit 6dad535d0df812c16ded2372e3dd818979367cb2.

--- utils/PipedExec.go
+++ utils/PipedExec.go
@@ -69,10 +69,6 @@ func (Self *PipedExec) Run(out io.Writer, err io.Writer) error {
 		if nil != err {
 			panic("Failed:" + cmd.cmd.Path)
 		}
-		err = cmd.cmd.Wait()
-		if nil != err {
-			return err
-		}
 	}
 
 	return Self.cmds[lastIdx].cmd.Wait()
