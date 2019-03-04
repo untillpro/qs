84eda803ae336fd3429214487966e729a74e011f
YALTA <maxim.ge@gmail.com>
2019-03-05 00:45:09 +0300

    semver

--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/untillpro/qs
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/coreos/go-semver v0.2.0
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.3.0
 )
--- utils/semver_test.go
+++ utils/semver_test.go
@@ -5,11 +5,34 @@ import (
 	"testing"
 
 	"github.com/blang/semver"
+	coreos "github.com/coreos/go-semver/semver"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestCoreOsSemver(t *testing.T) {
+	{
+		sv1 := coreos.New("1.2.3-SNAPSHOT")
+		assert.Equal(t, sv1.PreRelease, coreos.PreRelease("SNAPSHOT"))
+		log.Println("*** PreRelease", sv1.PreRelease, " Metadata", sv1.Metadata)
+		sv2 := coreos.New("1.2.4-SNAPSHOT")
+		log.Println("***sv2 ", sv2)
+		assert.True(t, sv1.LessThan(*sv2))
+		sv2.Patch += 1
+		log.Println("***sv2 ", sv2)
+
+		sv3 := coreos.New("1.2.4")
+		assert.Equal(t, sv3.PreRelease, coreos.PreRelease(""))
+	}
+
+}
 
 func TestBlangSemver(t *testing.T) {
 
-	v, _ := semver.Parse("0.0.1-alpha.preview.222+123.github")
-	log.Println("***", v)
+	// v, _ := semver.Parse("0.0.1-alpha.preview.222+123.github")
+	// log.Println("***", v.Pre, v.Build)
+
+	v, _ := semver.Parse("0.0.1-SNAPSHOT")
+	assert.Equal(t, v.Pre[0].VersionStr, "SNAPSHOT", "")
+	v.LTE(v)
 
 }
